352e8ec244f8e3aa918c3e3a3fd2ac2f9cb58fb1
Ghazi <ghazi@ghazi.im>
2016-07-02 21:57:51 +0300

    initialize repo

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT | 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+    console.log('Private route hit');
+    next();
+
+	},
+  logger: function (req, res, next) {
+    console.log('Request :' + new Date().toString() + ' '  + req.method + ' ' + req.originalUrl);
+  }
+};
+
+app.use(middleware.logger);
+app.get('/about', middleware.requireAuthentication, function (req, res){
+	res.send('About Us');
+})
+// 
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+	console.log('EXpress Server Started! on PORT ' + PORT);
+});
